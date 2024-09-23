7d98baf649b344b763bf0a7ce6566aae7da4ed57
JuliaPalna <palna.julia@gmail.com>
2024-09-23 22:30:33 +0300

    task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  const firstTruthValue = "1xBet".toLowerCase();
+  const secondTruthValue = "XXX".toLowerCase();
+
+  const stringLowerCase = str.toLowerCase();
+
+  const isIncludesFirstTruthValue = stringLowerCase.includes(firstTruthValue);
+  const isIncludesSecondTruthValue = stringLowerCase.includes(secondTruthValue);
+
+  return isIncludesFirstTruthValue || isIncludesSecondTruthValue;
 }
