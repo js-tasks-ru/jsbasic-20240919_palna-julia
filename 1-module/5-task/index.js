d0dd58a1fd077c9c6685b70658d7d68270ca7e76
JuliaPalna <palna.julia@gmail.com>
2024-09-28 22:27:39 +0300

    task 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -9,5 +9,3 @@ function truncate(str, maxlength) {
   const modifiedString = str.slice(0, maxlength - 1) + "…";
   return modifiedString;
 }
-
-console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
